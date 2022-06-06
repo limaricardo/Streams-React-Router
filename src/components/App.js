3b7c78eadbbf4382bb5be2db4c23a816ff764c04
Ricardo <limaricardop@gmail.com>
2022-06-06 18:48:09 -0300

    Components Scaffolding

---
+++ src/components/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { BrowserRouter, Route } from 'react-router-dom';
+
+const App = () => {
+    return (
+        <div>
+            <BrowserRouter>
+                <div>
+                    <Route path="/" exact component={PageOne} />
+                    <Route path="/pagetwo" component={PageTwo} />
+                </div>
+            </BrowserRouter>
+        </div>
+    )
+};
+
+export default App;
