a4a3eecfa3b0d9874f1c6ed7f823303f8b7b0956
Ricardo <limaricardop@gmail.com>
2022-06-19 21:05:24 -0300

    connect reducers

--- src/index.js
+++ src/index.js
@@ -2,12 +2,16 @@ import { configure } from "@testing-library/react";
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, applyMiddleware, compose } from "@reduxjs/toolkit";
 
 import App from "./components/App";
 import reducers from "./reducers";
 
-const store = configureStore({ reducer: reducers });
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = configureStore(
+  { reducer: reducers },
+  composeEnhancers(applyMiddleware())
+);
 
 ReactDOM.render(
   <Provider store={store}>
